e57412bd344079fc78b36b1fedc244efce030975
Jay <jerry.sirisavath@go.minneapolis.edu>
2023-10-28 19:51:49 -0500

    Configured module.exports

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-  devServer: {
-    // '/api/states e.g : http://127.0.0.1:3000/api/states http://127.0.0.1:3000/api/states
+// module.exports = {
+//   devServer: {
+//     // '/api/states e.g : http://127.0.0.1:3000/api/states http://127.0.0.1:3000/api/states
 
-    // For us, we want to  send our requests like getting api states to this vue dev-server address with /api/states
-    proxy: 'http://127.0.0.1:3000',
-  },
+//     // For us, we want to  send our requests like getting api states to this vue dev-server address with /api/states
+//     proxy: 'http://127.0.0.1:3000',
+//   },
+// };
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/50statesWebApp/' : '/',
 };
